e49c4ce557a4e253563f8b7d01c153a96f0a26e1
crawf <crawfordmikyle@gmail.com>
2021-05-02 17:47:01 -0700

    Added profile v and command

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # This is a Wifi Password Scanner
 # Mainly python practice
 # Here I am making sure get works ignore this
+
+# Start For Real
+# Import subprocess so we can access system commands
+import subprocess
+
+# Import Regex
+import re
+
+# Create a variable and store the list of wifi profiles
+profiles = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+

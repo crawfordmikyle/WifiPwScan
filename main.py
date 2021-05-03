dc8120c02f089f34b02142cbb04794149155e1e9
crawf <crawfordmikyle@gmail.com>
2021-05-02 18:25:06 -0700

    Done

--- main.py
+++ main.py
@@ -27,10 +27,25 @@ if len(profile_names) != 0:
         # Now we run more specific commands to get the information we need
         # about the wifi connection and see if there is a security key
         profile_info = subprocess.run(["netsh", "wlan", "show", "profile", n], capture_output= True).stdout.decode()
-        #print(profile_info)
+        # print(profile_info)
         # look for cases where there is no password so we can skip them
         if re.search("Security key           : Absent", profile_info):
             continue
         else:
             # Assign the SSID to the dictionary
             wifi_profile["ssid"] = n
+            # print(wifi_profile)
+            # Run key=clear to get the passwords
+            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profiles", n, "key=clear"], capture_output= True).stdout.decode()
+            # print(profile_info_pass)
+            # More Regex magic
+            password = re.search("Key Content            :(.*)\r",profile_info_pass)
+            # print(password)
+            # check if we found a password
+            if password == None:
+                wifi_profile["password"] = None
+            else:
+                wifi_profile["password"] = password[1]
+            wifi_list.append(wifi_profile)
+    for x in range(len(wifi_list)):
+        print(wifi_list[x])

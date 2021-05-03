5a4ec4af51c076d7e627f59052401d8193123a6d
crawf <crawfordmikyle@gmail.com>
2021-05-02 18:05:36 -0700

    working on the main section

--- main.py
+++ main.py
@@ -11,4 +11,20 @@ import re
 
 # Create a variable and store the list of wifi profiles
 profiles = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+# print(profiles)
+# Some Regex magic to return an array of all the wifi profile names from profiles
+profile_names = (re.findall("All User Profile     : (.*)\r", profiles))
+# print(profile_names)
+# Create empty array to store data
+wifi_list = []
+
+# Check to make sure you have gotten profiles and start working
+if len(profile_names) != 0:
+    for n in profile_names:
+        # Each wifi connection will need its own dictionary
+        # We will appened this to the wifi_list variable
+        wifi_profile = {}
+        # Now we run more specific commands to get the information we need
+        # about the wifi connection and see if there is a security key
+    profile_ info
 
